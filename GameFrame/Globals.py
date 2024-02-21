85ad8961ae09216c4ed02b236f6f9e3409748dfd
26FH <158244588+26FH@users.noreply.github.com>
2024-02-21 13:43:50 +1000

    Created GamePlay room.

--- GameFrame/Globals.py
+++ GameFrame/Globals.py
@@ -15,7 +15,7 @@ class Globals:
     window_name = 'Space Rescue'
 
     # - Set the order of the rooms - #
-    levels = ["WelcomeScreen", "GamePlay"]
+    levels = ["WelcomeScreen", "Gameplay"]
 
     # - Set the starting level - #
     start_level = 0
